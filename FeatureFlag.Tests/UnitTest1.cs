c307f0e60e061972039e8462678b5f56c6756eee
Bob Campbell <mostlybob@gmail.com>
2018-05-28 08:38:59 -0500

    stub in solution/project/tests structure

---
+++ FeatureFlag.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FeatureFlag.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ FeatureFlag/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FeatureFlag
+{
+    public class Class1
+    {
+    }
+}
