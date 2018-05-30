82a8a27af6ef3d90ee15a0065cb29fff5a9cecf5
Bob Campbell <mostlybob@gmail.com>
2018-05-30 18:07:25 -0500

    refactored provider to separate file

--- FeatureFlag/Class1.cs
+++ FeatureFlag/Class1.cs
@@ -1,30 +1,5 @@
-﻿using System;
-
-namespace FeatureFlag
+﻿namespace FeatureFlag
 {
-    public interface IFlagProvider
-    {
-        bool GetFlagSetting(string flagName);
-    }
-
-    public class FlagProvider : IFlagProvider
-    {
-        private readonly Func<IFeatureStore> featureStore;
-
-        public FlagProvider(Func<IFeatureStore> featureStore)
-        {
-            this.featureStore = featureStore;
-        }
-
-        public bool GetFlagSetting(string flagName)
-        {
-            var setting = featureStore().GetFeatureSetting(flagName);
-
-            return Convert.ToBoolean(setting);
-        }
-    }
-
-
     public interface IFeatureStore
     {
         string GetFeatureSetting(string featureName);
