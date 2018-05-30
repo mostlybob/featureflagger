64d7c3ab74c790f04b400fb330374085ea3b5c17
Bob Campbell <mostlybob@gmail.com>
2018-05-30 06:00:07 -0500

    refactor some tests for redundancy

--- FeatureFlag.Tests/UnitTest1.cs
+++ FeatureFlag.Tests/UnitTest1.cs
@@ -8,28 +8,31 @@ namespace FeatureFlag.Tests
 {
     public class ProviderTests
     {
-        string testKey;
+        private string testKey;
         private IFlagProvider providerTest;
-
+        private Mock<IFeatureStore> mockFeatureStore;
 
         public ProviderTests()
         {
-            var featureStoreMock = new Mock<IFeatureStore>();
-
-            featureStoreMock
-            .Setup(yy => yy.GetFeatureSetting(It.IsAny<string>()))
-            .Returns("True");
-
             testKey = Guid.NewGuid().ToString();
+        }
 
+        private void SetupMock(string key, string returnValue)
+        {
+            mockFeatureStore = new Mock<IFeatureStore>();
+
+            mockFeatureStore
+            .Setup(yy => yy.GetFeatureSetting(key))
+            .Returns(returnValue);
         }
 
         [Fact]
         public void It_should_return_false_for_non_existent_key()
         {
-            var featureStoreMock = new Mock<IFeatureStore>();
+            // var featureStoreMock = new Mock<IFeatureStore>();
+            SetupMock("bogus", null);
 
-            providerTest = new FlagProvider(featureStoreMock.Object);
+            providerTest = new FlagProvider(mockFeatureStore.Object);
 
             providerTest.GetFlagSetting("bogus").ShouldBeFalse();
         }
@@ -37,45 +40,33 @@ namespace FeatureFlag.Tests
         [Fact]
         public void It_should_call_the_store_with_the_key()
         {
-            var featureStoreMock = new Mock<IFeatureStore>();
+            SetupMock(It.IsAny<string>(), "True");
 
-            featureStoreMock
-            .Setup(yy => yy.GetFeatureSetting(It.IsAny<string>()))
-            .Returns("True");
-
-            providerTest = new FlagProvider(featureStoreMock.Object);
+            providerTest = new FlagProvider(mockFeatureStore.Object);
 
             providerTest.GetFlagSetting(testKey);
 
-            featureStoreMock.Verify(xx => xx.GetFeatureSetting(It.IsAny<string>()), Times.Once);
+            mockFeatureStore.Verify(xx => xx.GetFeatureSetting(It.IsAny<string>()), Times.Once);
         }
 
         [Fact]
         public void It_should_call_the_store_with_the_passed_key()
         {
-                       var featureStoreMock = new Mock<IFeatureStore>();
+            SetupMock(It.IsAny<string>(), "True");
 
-            featureStoreMock
-            .Setup(yy => yy.GetFeatureSetting(testKey))
-            .Returns("True");
-
-            providerTest = new FlagProvider(featureStoreMock.Object);
+            providerTest = new FlagProvider(mockFeatureStore.Object);
 
             providerTest.GetFlagSetting(testKey);
 
-            featureStoreMock.Verify(xx => xx.GetFeatureSetting(testKey), Times.Once);
+            mockFeatureStore.Verify(xx => xx.GetFeatureSetting(testKey), Times.Once);
         }
 
         [Fact]
         public void It_should_return_true_when_store_returns_a_true_string()
         {
-            var featureStoreMock = new Mock<IFeatureStore>();
+            SetupMock(testKey, "True");
 
-            featureStoreMock
-            .Setup(yy => yy.GetFeatureSetting(testKey))
-            .Returns("True");
-
-            providerTest = new FlagProvider(featureStoreMock.Object);
+            providerTest = new FlagProvider(mockFeatureStore.Object);
 
             providerTest.GetFlagSetting(testKey).ShouldBeTrue();
         }
