85d1c9c57985ca3a39a64ba3815261abe6e59319
Bob Campbell <mostlybob@gmail.com>
2018-05-29 21:57:51 -0500

    added test for calling the store with an existing key

--- FeatureFlag.Tests/UnitTest1.cs
+++ FeatureFlag.Tests/UnitTest1.cs
@@ -8,6 +8,7 @@ namespace FeatureFlag.Tests
 {
     public class ProviderTests
     {
+        string testKey;
         private IFlagProvider providerTest;
 
 
@@ -18,22 +19,24 @@ namespace FeatureFlag.Tests
             featureStoreMock
             .Setup(yy => yy.GetFeatureSetting(It.IsAny<string>()))
             .Returns("True");
+
+            testKey = Guid.NewGuid().ToString();
+
         }
 
         [Fact]
         public void It_should_return_false_for_non_existent_key()
         {
             var featureStoreMock = new Mock<IFeatureStore>();
-            
-            providerTest=new FlagProvider(featureStoreMock.Object);
-            
+
+            providerTest = new FlagProvider(featureStoreMock.Object);
+
             providerTest.GetFlagSetting("bogus").ShouldBeFalse();
         }
 
         [Fact]
         public void It_should_call_the_store_with_the_key()
         {
-            var key = System.Guid.NewGuid().ToString();
             var featureStoreMock = new Mock<IFeatureStore>();
 
             featureStoreMock
@@ -42,9 +45,23 @@ namespace FeatureFlag.Tests
 
             providerTest = new FlagProvider(featureStoreMock.Object);
 
-            providerTest.GetFlagSetting(key);
+            providerTest.GetFlagSetting(testKey);
 
-            featureStoreMock.Verify(xx=>xx.GetFeatureSetting(It.IsAny<string>()),Times.Once);
+            featureStoreMock.Verify(xx => xx.GetFeatureSetting(It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact]
+        public void It_should_return_true_when_store_returns_a_true_string()
+        {
+            var featureStoreMock = new Mock<IFeatureStore>();
+
+            featureStoreMock
+            .Setup(yy => yy.GetFeatureSetting(testKey))
+            .Returns("True");
+
+            providerTest = new FlagProvider(featureStoreMock.Object);
+
+            providerTest.GetFlagSetting(testKey).ShouldBeTrue();
         }
 
     }
